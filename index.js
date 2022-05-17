b0985fa2d397adbf7be37681e84b17ef2cc9686f
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-05-17 15:05:03 +0500

    updated base structure

--- index.js
+++ index.js
@@ -5,18 +5,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')))
 
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || "3000";
 
 var sampleRoute = require('./routes/sampleRoute')
 
 app.get("/", (req, res) => {
     res.status(200).send("This is basic template.");
 });
-
-//Replace with your.
-app.use("/template",sampleRoute);
-
-
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
